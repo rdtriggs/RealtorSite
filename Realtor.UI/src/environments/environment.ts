3b0d8c2191440d24bec40c3ca9534d6d65a8c0bd
Ryan Triggs <rdtriggs@gmail.com>
2020-10-01 21:54:07 -0700

    Update environment.ts

--- Realtor.UI/src/environments/environment.ts
+++ Realtor.UI/src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: false,
-  
+  firebaseConfig: {
+    apiKey: "AIzaSyBzLnccmVUi_QQ0nLtdrChmZ9NagqQbNnE",
+    authDomain: "realtorsite-52775.firebaseapp.com",
+    databaseURL: "https://realtorsite-52775.firebaseio.com",
+    projectId: "realtorsite-52775",
+    storageBucket: "realtorsite-52775.appspot.com",
+    messagingSenderId: "722451059643",
+    appId: "1:722451059643:web:21fc781e780d2eec4f2a9e",
+    measurementId: "G-V159RV87ZD"
+  },
   apiUrl: "http://localhost:5000/api"
 };
